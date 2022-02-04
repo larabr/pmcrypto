254baa97b0603be934fd596a804d11ac23f4703f
larabr <7375870+larabr@users.noreply.github.com>
2022-02-04 15:45:14 +0100

    Upgrade prettier for proper TS support

--- lib/pmcrypto.d.ts
+++ lib/pmcrypto.d.ts
@@ -34,7 +34,7 @@ export type OpenPGPKey = Key;
 export type OpenPGPMessage = Message<Uint8Array | string>; // TODO missing streaming support
 export type OpenPGPSignature = Signature;
 
-export { generateKey, reformatKey }
+export { generateKey, reformatKey };
 
 export interface DecryptLegacyOptions extends Omit<DecryptOptions, 'message'> {
     message: string;
@@ -111,10 +111,10 @@ export interface DecryptOptionsPmcrypto extends DecryptOptions {
 }
 
 export type DecryptResultPmcrypto = Omit<DecryptMessageResult, 'signatures'> & {
-    signatures: (OpenPGPSignature)[]; // Promise if streamed input
+    signatures: OpenPGPSignature[]; // Promise if streamed input
     verified: VERIFICATION_STATUS; // Promise if streamed input
     errors?: Error[]; // Promise if streamed input
-}
+};
 
 export function decryptMessage(
     options: DecryptOptionsPmcrypto & { format: 'utf8' }
@@ -126,9 +126,7 @@ export function decryptMessage(options: DecryptOptionsPmcrypto): Promise<Decrypt
 
 export function decryptMessageLegacy(options: DecryptLegacyOptions): Promise<DecryptResultPmcrypto>;
 
-export function decryptMIMEMessage(
-    options: DecryptMimeOptions
-): Promise<{
+export function decryptMIMEMessage(options: DecryptMimeOptions): Promise<{
     getBody: () => Promise<{ body: string; mimetype: string } | undefined>;
     getAttachments: () => Promise<any>;
     getEncryptedSubject: () => Promise<string>;
@@ -226,9 +224,7 @@ export function getSignature(option: string | Uint8Array | OpenPGPSignature): Pr
 
 export function getMessage(message: OpenPGPMessage | Uint8Array | string): Promise<OpenPGPMessage>;
 
-export function splitMessage(
-    message: OpenPGPMessage | Uint8Array | string
-): Promise<{
+export function splitMessage(message: OpenPGPMessage | Uint8Array | string): Promise<{
     asymmetric: Uint8Array[];
     signature: Uint8Array[];
     symmetric: Uint8Array[];
@@ -240,7 +236,7 @@ export function splitMessage(
 
 export function armorBytes(value: Uint8Array | string): Promise<Uint8Array | string>;
 
-export interface algorithmInfo {
+export interface AlgorithmInfo {
     algorithm: string;
     bits?: number; // if algorithm == 'rsaEncryptSign' | 'rsaEncrypt' | 'rsaSign'
     curve?: string; // if algorithm == 'ecdh' | 'eddsa' | 'ecdsa'
@@ -266,7 +262,7 @@ export interface VerifyMessageResult {
     data: openpgp_VerifyMessageResult['data'];
     verified: VERIFICATION_STATUS;
     signatures: OpenPGPSignature[];
-    signatureTimestamp: Date|null,
+    signatureTimestamp: Date | null;
     errors?: Error[];
 }
 export function verifyMessage(options: VerifyOptionsPmcrypto): Promise<VerifyMessageResult>;
@@ -305,7 +301,7 @@ export function processMIME(options: ProcessMIMEOptions): Promise<ProcessMIMERes
 
 export function serverTime(): Date;
 
-export function getSHA256Fingerprints(key: OpenPGPKey): Promise<string[]>
+export function getSHA256Fingerprints(key: OpenPGPKey): Promise<string[]>;
 
 export function canKeyEncrypt(key: OpenPGPKey, date?: Date): Promise<boolean>;
 
