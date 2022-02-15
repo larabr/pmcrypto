a34f8a809a318e3399c09bb445d19b7a48bc8440
larabr <7375870+larabr@users.noreply.github.com>
2022-02-15 17:53:17 +0100

    Accept key references in encrypt/decrypt/sign/verifyMessage

--- lib/pmcrypto.d.ts
+++ lib/pmcrypto.d.ts
@@ -299,6 +299,7 @@ export interface ProcessMIMEResult {
 export function processMIME(options: ProcessMIMEOptions): Promise<ProcessMIMEResult>;
 
 export function serverTime(): Date;
+export function updateServerTime(serverDate: Date): Date;
 
 export function getSHA256Fingerprints(key: OpenPGPKey): Promise<string[]>;
 
